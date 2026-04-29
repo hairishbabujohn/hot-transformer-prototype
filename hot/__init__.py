f54bb67fe84996dcd896545dc32b07e2bce50036
Harish Babu <hairishbabujohn@users.noreply.github.com>
2026-04-29 10:39:19 +0530

    Fix ImportError: remove obsolete compute_token_entropy_variance from __init__.py

--- hot/__init__.py
+++ hot/__init__.py
@@ -1,8 +1,8 @@
 """
 Homeostatic Transformer (HoT) package.
 """
-from .layers import HoTLayer, DepthwiseSepConv1d, compute_token_entropy_variance
+from .layers import HoTLayer, DepthwiseSepConv1d
 from .model import HoTEncoder
 from .czu import CZU
 
-__all__ = ["HoTLayer", "DepthwiseSepConv1d", "compute_token_entropy_variance", "HoTEncoder", "CZU"]
+__all__ = ["HoTLayer", "DepthwiseSepConv1d", "HoTEncoder", "CZU"]
